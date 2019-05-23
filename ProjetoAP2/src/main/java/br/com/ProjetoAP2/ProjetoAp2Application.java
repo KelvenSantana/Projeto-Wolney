3ebfe0dec4190dbb1e67d819a92c6c8c1228cb3c
KelvenSantana <kelven.santana022@gmail.com>
2019-05-23 20:21:21 -0300

    libdgel

---
+++ ProjetoAP2/src/main/java/br/com/ProjetoAP2/ProjetoAp2Application.java
@@ -0,0 +1,13 @@
+package br.com.ProjetoAP2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAp2Application.class, args);
+	}
+
+}
